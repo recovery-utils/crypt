05b086f3b7bb713b0558128bc2942a70e53f228c
recovery-utils <147285154+recovery-utils@users.noreply.github.com>
2023-10-07 22:27:43 -0400

    set local deployment

--- src/helpers/formatName.js
+++
@@ -1,8 +0,0 @@
-export const formatName = (fileName) => {
-  //remove .enc
-  let trimmed = fileName.replace(".enc", "");
-  //remove parenthesis
-  let clean = trimmed.replace(/ *\([^)]*\) */g, "");
-
-  return clean;
-};
